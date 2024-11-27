aaa729eb0299b60e8d30762224a5605dc2c94a17
Rofiatoladele <rofiatoladele2017@gmail.com>
2024-11-27 01:49:42 -0800

    Update config.ts
    meta title 
    meta description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Oladele Rofiat| Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Oladele Rofiat is a Digital Marketer Specialist who help business owner build there brand a website with Blog;
+export const SITE_DESCRIPTION = 'Oladele Rofiat is a Digital Marketer Specialist who help business owner build there brand a website;
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
